c0e0f1e87c147e5b41e975e0ce0fdd6d2bf47ca4
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-10-06 16:39:33 -0400

    Clone in setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ echo "Enter your jcluser password:"
 read -s WORKER_PASS
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible -y
+sudo apt-get install git ansible -y
+sudo -u $(logname) git clone --branch v0.0.1 --depth 1 https://github.com/jrokeach/contrail-for-mcms.git
 sudo ansible-galaxy collection install community.general community.crypto ansible.posix
 sudo bash -c '( cd k8s-setup ; ansible-playbook master-stage1.yml )'
 sudo -u $(logname) bash -c "( cd k8s-setup ; ansible-playbook worker.yml  --extra-vars 'ansible_sudo_pass=$WORKER_PASS ansible_pass=$WORKER_PASS' )"

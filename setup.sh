c6b21e38220caaa9a28fe9dab35413ff762a2245
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-10-06 16:44:49 -0400

    Minor structure update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ read -s WORKER_PASS
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install git ansible -y
-sudo -u $(logname) git clone --branch v0.0.1 --depth 1 https://github.com/jrokeach/contrail-for-mcms.git
+sudo -u $(logname) git clone --branch v0.0.1 --depth 1 https://github.com/jrokeach/cn24mcms.git
+cd cn24mcms
 sudo ansible-galaxy collection install community.general community.crypto ansible.posix
 sudo bash -c '( cd k8s-setup ; ansible-playbook master-stage1.yml )'
 sudo -u $(logname) bash -c "( cd k8s-setup ; ansible-playbook worker.yml  --extra-vars 'ansible_sudo_pass=$WORKER_PASS ansible_pass=$WORKER_PASS' )"

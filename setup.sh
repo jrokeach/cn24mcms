142b40ac4311bde2cd016ae838a403ca7f9ad199
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-10-06 17:00:40 -0400

    Permissions fixes

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install git ansible -y
 sudo -u $(logname) git clone --branch v0.0.1 --depth 1 https://github.com/jrokeach/cn24mcms.git
 cd cn24mcms
 sudo ansible-galaxy collection install community.general community.crypto ansible.posix
-sudo bash -c '( cd k8s-setup ; ansible-playbook master-stage1.yml )'
+sudo -u $(logname) bash -c "( cd k8s-setup ; ansible-playbook master-stage1.yml --extra-vars 'ansible_sudo_pass=$WORKER_PASS')"
 sudo -u $(logname) bash -c "( cd k8s-setup ; ansible-playbook worker.yml  --extra-vars 'ansible_sudo_pass=$WORKER_PASS ansible_ssh_pass=$WORKER_PASS' )"
 sudo -u $(logname) bash -c '( cd ../kubespray ; ansible-playbook cluster.yml )'
 sudo -u $(logname) bash -c '( cd k8s-setup ; ansible-playbook master-stage2.yml )'

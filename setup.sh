445f9193cc5baad6912a55c715e54694b5c3e424
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-10-07 15:00:03 -0400

    Point to right branch/release

--- setup.sh
+++ setup.sh
@@ -7,18 +7,14 @@ set -a
 read -sp "jcluser password: " WORKER_PASS
 echo -e "\r\nPlease note that the following IP addresses are all the management IP addresses.\r\nService (eth1) IP addresses are currently statically defined in master-stage1.yml > 'Set up ansible hosts file'."
 read -p "k8s-master mgt ip [100.123.35.1]: " k8s_master_mgtip
-k8s_master_mgtip=${k8s_master_mgtip:-100.123.35.1}
 read -p "k8s-worker1 mgt ip [100.123.35.2]: " k8s_worker1_mgtip
-k8s_worker1_mgtip=${k8s_worker1_mgtip:-100.123.35.2}
 read -p "k8s-worker2 mgt ip [100.123.35.3]: " k8s_worker2_mgtip
-k8s_worker2_mgtip=${k8s_worker2_mgtip:-100.123.35.3}
 read -p "k8s-worker3 mgt ip [100.123.35.4]: " k8s_worker3_mgtip
-k8s_worker3_mgtip=${k8s_worker3_mgtip:-100.123.35.4}
 echo
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install git ansible -y
-sudo -u $(logname) git clone --branch v0.0.1 --depth 1 https://github.com/jrokeach/cn24mcms.git
+sudo -u $(logname) git clone --branch v0.0.2_dev --depth 1 https://github.com/jrokeach/cn24mcms.git
 cd cn24mcms
 sudo ansible-galaxy collection install community.general community.crypto ansible.posix
 sudo -u $(logname) bash -c "(

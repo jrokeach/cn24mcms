385c8e45892630df6fd5e9c581a23bac376db449
Jason R. Rokeach <1076569+jrokeach@users.noreply.github.com>
2022-10-07 15:12:52 -0400

    Reduced sudo usage

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]
+  then echo "Please run as jcluser"
+  exit
+fi
+
 # Collect inputs
 # Note that this currently only supports a statically assumed 1 master and 3 workers.
 # Ideally, this should be taken from a configuration file along with service IP address information.
@@ -14,16 +19,16 @@ echo
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install git ansible -y
-sudo -u $(logname) git clone --branch v0.0.2_dev --depth 1 https://github.com/jrokeach/cn24mcms.git
+git clone --branch v0.0.2_dev --depth 1 https://github.com/jrokeach/cn24mcms.git
 cd cn24mcms
 sudo ansible-galaxy collection install community.general community.crypto ansible.posix
-sudo -u $(logname) bash -c "(
+bash -c "(
 	cd k8s-setup ;
 	ansible-playbook master-stage1.yml --extra-vars 'ansible_sudo_pass=$WORKER_PASS k8s_master_mgtip=$k8s_master_mgtip k8s_worker1_mgtip=$k8s_worker1_mgtip k8s_worker2_mgtip=$k8s_worker2_mgtip k8s_master_mgtip=$k8s_worker2_mgtip
 ')"
-sudo -u $(logname) bash -c "(
+bash -c "(
         cd k8s-setup ;
         ansible-playbook worker.yml  --extra-vars 'ansible_sudo_pass=$WORKER_PASS ansible_ssh_pass=$WORKER_PASS'
 )"
-sudo -u $(logname) bash -c '( cd ../kubespray ; ansible-playbook cluster.yml )'
-sudo -u $(logname) bash -c '( cd k8s-setup ; ansible-playbook master-stage2.yml )'
+bash -c '( cd ../kubespray ; ansible-playbook cluster.yml )'
+bash -c '( cd k8s-setup ; ansible-playbook master-stage2.yml )'
